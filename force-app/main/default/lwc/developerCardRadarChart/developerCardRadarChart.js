c2ea636bb066f6864589be3863d3a77ffbcff375
Evan Pollack <epollack@salesforce.com>
2023-02-15 20:17:19 -0500

    copied basic structure from test

--- force-app/main/default/lwc/developerCardRadarChart/developerCardRadarChart.js
+++ force-app/main/default/lwc/developerCardRadarChart/developerCardRadarChart.js
@@ -1,3 +1,28 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
+import { loadScript } from 'lightning/platformResourceLoader';
+import CHARTJS from '@salesforce/resourceUrl/ChartJS';
 
-export default class DeveloperCardRadarChart extends LightningElement {}
+export default class DeveloperCardRadarChart extends LightningElement {
+    @api skills; // expected to be am array of data
+    
+    renderedCallback() {
+        loadScript(this, `${CHARTJS}/chart.js`)
+            .then(() => this.initializeChart())
+            .catch((e) => console.error('Failed to load chart.js', e.message));
+    }
+
+    initializeChart() {
+        const ctx = this.template.querySelector('canvas');
+
+        new Chart(ctx, {
+            type: 'radar',
+            data: {
+                labels: [],
+                datasets: [{
+                    label: 'Ratio Description',
+                    data: [],
+                }]
+            }
+        });
+    }
+}

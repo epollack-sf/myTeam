5f75583e3b2e093c941a3a42e7ed028b91cc9852
Evan Pollack <epollack@salesforce.com>
2023-02-16 12:19:12 -0500

    added logic to get top categories

--- force-app/main/default/lwc/developerCardRadarChart/developerCardRadarChart.js
+++ force-app/main/default/lwc/developerCardRadarChart/developerCardRadarChart.js
@@ -2,9 +2,29 @@ import { LightningElement, api } from 'lwc';
 import { loadScript } from 'lightning/platformResourceLoader';
 import CHARTJS from '@salesforce/resourceUrl/ChartJS';
 
+const MAX_LEVEL = 5;
 export default class DeveloperCardRadarChart extends LightningElement {
-    @api skills; // expected to be am array of data
-    
+    @api skills; // expected to be am array of objects 
+    // {id: '', Type__c: '', Category__c: '', Name: '', Rating__c: 2}
+
+    get top8Categories() {
+        let totalCategories = this.skills.map(skill => skill.Category__c);
+        let uniqueCategories = Array.from(new Set(totalCategories));
+        
+        if (uniqueCategories.length <= 8) {
+            return uniqueCategories;
+        } else {
+            let counts = [];
+            
+            for (category of uniqueCategories) {
+                counts.push({ category: category, count: totalCategories.filter(c => c === category).length });
+            }
+            counts.sort((a, b) => b.count - a.count);
+            
+            return counts.map(o => o.category).slice(0, 8);
+        }
+    }
+
     renderedCallback() {
         loadScript(this, `${CHARTJS}/chart.js`)
             .then(() => this.initializeChart())
@@ -17,7 +37,7 @@ export default class DeveloperCardRadarChart extends LightningElement {
         new Chart(ctx, {
             type: 'radar',
             data: {
-                labels: [],
+                labels: [...this.top8Categories],
                 datasets: [{
                     label: 'Ratio Description',
                     data: [],

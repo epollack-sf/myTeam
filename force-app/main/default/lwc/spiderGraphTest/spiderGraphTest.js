c015da4914391f25a9db84a1dec8cda765694c31
Evan Pollack <epollack@salesforce.com>
2023-02-16 12:19:39 -0500

    modified test

--- force-app/main/default/lwc/spiderGraphTest/spiderGraphTest.js
+++ force-app/main/default/lwc/spiderGraphTest/spiderGraphTest.js
@@ -1,8 +1,21 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import { loadScript } from 'lightning/platformResourceLoader';
 import ChartJS from '@salesforce/resourceUrl/ChartJS';
 
+/*
+import { getRecords } from 'lightning/uiRecordApi';
+import NAME_FIELD from '@salesforce/schema/Contact.Name';
+import EMAIL_FIELD from '@salesforce/schema/Contact.Email';
+*/
+
 export default class SpiderGraphTest extends LightningElement {
+    /*@wire(getRecords, {
+        records: {
+            recordIds: ['003Dn000007J3I8IAK', '003Dn000007J3IFIA0', '003Dn000007J3IIIA0'],
+            fields: [NAME_FIELD]
+        }
+    }) testContacts;*/
+    
     renderedCallback() {
         loadScript(this, `${ChartJS}/chart.js`)
             .then(() => this.initializeSpiderChart())
@@ -15,13 +28,17 @@ export default class SpiderGraphTest extends LightningElement {
         new Chart(ctx, {
             type: 'radar',
             data: {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Indigo', 'Violet', 'Cheese'],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 1, 5, 15],
-                borderWidth: 1
-            }]
+                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Indigo', 'Violet', 'Cheese'],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 1, 5, 15],
+                    borderWidth: 1
+                }]
             },
         });
     }
+
+    /*displayTest(){
+        console.log(JSON.stringify(this.testContacts));
+    }*/
 }
